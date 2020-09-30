7e9d012ac9248c5baf16d56a82d3308eb01c2ab3
yuki sakuma <kumasan.04.24.14@gmail.com>
2020-09-30 21:21:31 +0900

    [Improve] memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -3,15 +3,18 @@
 void
 *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n)
 {
-	unsigned char *dst_c;
-	const unsigned char *src_c;
+	char *dst_c;
+	const char *src_c;
 
 	dst_c = dst;
 	src_c = src;
 	while (n--)
 	{
 		if (*src_c == c)
+		{
+			*dst_c = *src_c;
 			return (++dst_c);
+		}
 		*dst_c++ = *src_c++;
 	}
 	return (NULL);
