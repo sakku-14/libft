56f92aac3d13e9b72cd7f524a6b1181bbf0cb82f
yuki sakuma <kumasan.04.24.14@gmail.com>
2020-10-01 09:18:31 +0900

    [Improve] ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -5,20 +5,31 @@ void	*memcpy_rev(void *dst, const void *src, size_t len)
 	char *dst_c;
 	const char *src_c;
 
-	dst_c = dst;
-	src_c = src;
-	while (len > 0)
-	{
-		dst_c[len - 1] = src_c[len - 1];
-		len--;
-	}
+	dst_c = dst + len - 1;
+	src_c = src + len - 1;
+	while (len--)
+		*dst_c-- = *src_c--;
+	return (dst);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char		*tmp;
+	const char	*src_cpy;
+
+	tmp = dst;
+	src_cpy = src;
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	while (n--)
+		*tmp++ = *src_cpy++;
 	return (dst);
 }
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	if (dst <= src)
-		return (ft_memcpy(dst, src, len));
-	else
+	if (dst > src)
 		return (memcpy_rev(dst, src, len));
+	else
+		return (ft_memcpy(dst, src, len));
 }
