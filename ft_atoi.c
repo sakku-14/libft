acd38ea4cbf2e9ffc619b34e40a54de87b94576a
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-04 14:32:50 +0900

    [Add

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,6 +1,25 @@
 #include "libft.h"
-
 int	ft_atoi(const char *str)
 {
-	
+	int num;
+	int flag;
+
+	num = 0;
+	flag = 0;
+	while (('\t' <= *str && *str <= '\r') || *str == ' ')
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			flag = 1;
+		str++;
+	}
+	while ('0' <= *str && *str <= '9')
+	{
+		num = num * 10 - (*str - '0');
+		str++;
+	}
+	if (flag == 1)
+		return (num);
+	return (-num);
 }
--- libft.h
+++ libft.h
@@ -17,6 +17,6 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-//int		ft_atoi(const char *str);
+int		ft_atoi(const char *str);
 
 #endif
