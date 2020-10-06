8d0fd5adfc09167ec7ffb8843b4e7a2ab2191ccb
Yuki Sakuma <sakku.0210.14@gmail.com>
2020-10-06 18:50:52 +0900

    [Improve] some

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -13,9 +13,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		ft_strlcat(str, s1, ft_strlen(s1) + 1);
 		ft_strlcat(str, s2, len + 1);
 	}
-	else if (!*s1)
+	else if (!*s1 && *s2)
 		ft_strlcat(str, s2, ft_strlen(s2) + 1);
-	else
+	else if (*s1 && !*s2)
 		ft_strlcat(str, s1, ft_strlen(s1) + 1);
+	else
+		return (NULL);
 	return (str);
 }
