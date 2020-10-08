e98d400be04eaaed1442a50e0d0c461859f136a8
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-08 16:41:37 +0900

    [Add] strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char *ret;
+	int len;
+
+	len = ft_strlen(s);
+	if (!(ret = (char *)malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	while (*s)
+	{
+		*ret = f(1, *s);
+		ret++;
+		s++;
+	}
+	while (len--)
+		ret--;
+	return (ret);
+}
