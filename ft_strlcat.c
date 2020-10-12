986ad31e1602e5a3da0c89ff034c3350e082a3db
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-12 11:27:48 +0900

    [Improve] strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,6 +3,20 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t len;
+	size_t room;
+
+	len = ft_strlen(dst) + ft_strlen(src);
+	room = dstsize - 1 - ft_strlen(dst);
+	while (*dst)
+		dst++;
+	if (room >= 0)
+		ft_strlcpy(dst, src, room + 1);
+	if (ft_strlen(dst) > dstsize)
+		return (ft_strlen(dst) + 1);
+	return (len);
+
+	/*
+	size_t len;
 
 	if (dstsize < ft_strlen(dst))
 	{
@@ -19,4 +33,5 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	}
 	ft_strlcpy(dst, src, dstsize);
 	return (len);
+	*/
 }
