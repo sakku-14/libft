9116cf6603887e12d088acd160795b6c8f204564
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-12 17:49:32 +0900

    [Improve] strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,25 +3,27 @@
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char *ret;
-	char *ret_c;
-	char *set_c;
+	size_t i;
+	size_t j;
+	size_t k;
+	size_t len;
 
-	if (!(ret = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1))))
+	i = 0;
+	k = 0;
+	j = ft_strlen(s1);
+	while ((ft_strchr(set, s1[i]) != NULL) && i < j)
+		i++;
+	while ((ft_strchr(set, s1[j]) != NULL) && j > 0)
+		j--;
+	if (j == 0)
+		len = 0;
+	else
+		len = j - i + 1;
+	if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	ret_c = ret;
-	set_c = (char *)set;
-	while (*s1)
-	{
-		while (*set_c)
-		{
-			if (*s1 == *set_c)
-				break ;
-			set_c++;
-		}
-		if (!*set_c)
-			*ret++ = *s1;
-		set_c = (char *)set;
-		s1++;
-	}
-	return (ret_c);
+	ret[0] = '\0';
+	while (i <= j)
+		ret[k++] = s1[i++];
+	ret[k] = '\0';
+	return (ret);
 }
