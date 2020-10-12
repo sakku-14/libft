dd49b050d0b3ebf2113f7caa15f26735b822833f
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-12 15:24:05 +0900

    [Improve] strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -2,22 +2,24 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char *str;
-	int len;
+	size_t len;
+	size_t i;
+	size_t j;
+	char *ret;
 
 	len = ft_strlen(s1) + ft_strlen(s2);
-	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
+	if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	if (*s1 && *s2)
+	ret[0] = '\0';
+	i = 0;
+	while (i < ft_strlen(s1))
 	{
-		ft_strlcat(str, s1, ft_strlen(s1) + 1);
-		ft_strlcat(str, s2, len + 1);
+		ret[i] = s1[i];
+		i++;
 	}
-	else if (!*s1 && *s2)
-		ft_strlcat(str, s2, ft_strlen(s2) + 1);
-	else if (*s1 && !*s2)
-		ft_strlcat(str, s1, ft_strlen(s1) + 1);
-	else
-		return (NULL);
-	return (str);
+	j = 0;
+	while (j < ft_strlen(s2))
+		ret[i++] = s2[j++];
+	ret[i] = '\0';
+	return (ret);
 }
