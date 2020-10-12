46585b65668d75071a82d9d6c883b082a4987aeb
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-12 21:04:45 +0900

    [Improve] itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,67 +5,53 @@ int check_negative(int n)
 	int flag;
 
 	flag = 0;
-	if (n < 0)
+	if (n >= 0)
 		flag = 1;
 	return (flag);
 }
 
-int digit_is(int n)
+int digit_is(int n, int flag)
 {
 	int digit;
 
 	digit = 0;
 	if (n == 0)
 		digit = 1;
-	while (n > 0)
+	while (n <= -1)
 	{
 		n /= 10;
 		digit++;
 	}
+	if (flag == 0)
+		return(digit + 1);
 	return (digit);
 }
 
-void set_rnum(int n, char *r_num)
+void set_num(int flag, int digit, char *num, int n)
 {
-	if (n == 0)
-		*r_num++ = '0'; 
-	while (n > 0)
+	num[digit--] = '\0';
+	while (digit > 0)
 	{
-		*r_num++ = n % 10 + '0';
+		num[digit--] = -(n % 10) + '0';
 		n /= 10;
 	}
-}
-
-void set_num(int flag, int digit, char *num, char *r_num)
-{
-	int i;
-
-	i = 0;
-	if (flag == 1)
-		*num++ = '-';
-	while (++i < digit)
-		r_num++;
-	while (digit--)
-		*num++ = *r_num--;
+	if (flag == 0)
+		num[digit] = '-';
+	else
+		num[digit] = -(n % 10) + '0';
 }
 
 char *ft_itoa(int n)
 {
 	char *num;
-	char *num_c;
-	char *r_num;
 	int digit;
 	int flag;
 
 	if ((flag = check_negative(n)))
 		n *= -1;
-	digit = digit_is(n);
-	if (!(num = (char *)malloc(sizeof(char) * digit)))
+	digit = digit_is(n, flag);
+	if (!(num = (char *)malloc(sizeof(char) * (digit + 1))))
 		return (NULL);
-	num_c = num;
-	if (!(r_num = (char *)malloc(sizeof(char) * digit)))
-		return (NULL);
-	set_rnum(n, r_num);
-	set_num(flag, digit, num, r_num);
-	return (num_c);
+	set_num(flag, digit, num, n);
+	return (num);
 }
