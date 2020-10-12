35e2e60d95105dcc40c39851a418a74d6f3af279
Yuki Sakuma <sakku.0210.14@gmail.com>
2020-10-12 12:33:13 +0900

    [Improve] strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -5,33 +5,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t len;
 	size_t room;
 
-	len = ft_strlen(dst) + ft_strlen(src);
-	room = dstsize - 1 - ft_strlen(dst);
-	while (*dst)
-		dst++;
-	if (room >= 0)
-		ft_strlcpy(dst, src, room + 1);
-	if (ft_strlen(dst) > dstsize)
-		return (ft_strlen(dst) + 1);
-	return (len);
-
-	/*
-	size_t len;
-
 	if (dstsize < ft_strlen(dst))
 	{
-		len = ft_strlen(dst) + 1;
+		len = ft_strlen(src) + dstsize;
 		return (len);
 	}
 	else
 		len = ft_strlen(dst) + ft_strlen(src);
-
+	room = dstsize - 1 - ft_strlen(dst);
 	while (*dst)
-	{
-		dstsize--;
 		dst++;
-	}
-	ft_strlcpy(dst, src, dstsize);
+	ft_strlcpy(dst, src, room + 1);
 	return (len);
-	*/
 }
