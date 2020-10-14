874a2525815b94760f8cd4673502c2603a1fc47e
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-14 11:13:19 +0900

    [Improve] strncmp & lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 22:50:34 by ysakuma           #+#    #+#             */
-/*   Updated: 2020/10/13 23:39:55 by ysakuma          ###   ########.fr       */
+/*   Updated: 2020/10/14 11:12:26 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	t_list *tmp;
+
+	tmp = *lst;
+	if (!new)
+		return ;
 	if (*lst)
 	{
-		ft_lstlast(*lst);
-		(*lst)->next = new;
+		tmp = ft_lstlast(*lst);
+		tmp->next = new;
 	}
 	else
 		*lst = new;
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 22:54:57 by ysakuma           #+#    #+#             */
-/*   Updated: 2020/10/13 22:54:58 by ysakuma          ###   ########.fr       */
+/*   Updated: 2020/10/14 10:41:23 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (!*s1 || !*s2)
-		return ((unsigned char)*s1 - (unsigned char)*s2);
-	while (*s1 && *s2 && n--)
+	while (n--)
 	{
 		if (*s1 != *s2)
 			return ((unsigned char)*s1 - (unsigned char)*s2);
-		s1++;
+		if (!*s1++)
+			break ;
 		s2++;
 	}
 	return (0);
