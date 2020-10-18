6be0ba8314a2df9560c3451db94bd2e65f1fdef1
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-18 15:09:46 +0900

    [Improve] strnstr delete this_strncmp

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,27 +6,11 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 22:55:15 by ysakuma           #+#    #+#             */
-/*   Updated: 2020/10/14 13:36:40 by ysakuma          ###   ########.fr       */
+/*   Updated: 2020/10/18 15:09:15 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	this_strncmp(const char *s1, const char *s2, size_t n)
-{
-	while (n--)
-	{
-		if (!*s2)
-			return (0);
-		if (*s1 != *s2)
-			break ;
-		s1++;
-		s2++;
-		if (!*s1 && !*s2)
-			return (0);
-	}
-	return (1);
-}
 
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -35,8 +19,12 @@ char		*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	while (*haystack && len)
 	{
 		if (*haystack == *needle)
-			if (this_strncmp(haystack, needle, len) == 0)
+		{
+			if (ft_strlen(needle) > len)
+				return (NULL);
+			if (ft_strncmp(haystack, needle, ft_strlen(needle)) == 0)
 				return ((char *)haystack);
+		}
 		haystack++;
 		len--;
 	}
